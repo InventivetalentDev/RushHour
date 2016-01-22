9ae6adc89f883dde4d3ea862a2b37d59dc848a71
inventivetalent <inventivetalent@inventivegames.de>
2016-01-22 12:07:12 +0100

    add toString, equals and hashCode to Car

--- RushHour/src/org/inventivetalent/rushhour/car/Car.java
+++ RushHour/src/org/inventivetalent/rushhour/car/Car.java
@@ -13,4 +13,31 @@ public class Car {
 		this.rotation = rotation;
 	}
 
+	@Override
+	public String toString() {
+		return "Car[v=" + variant.name() + ",r=" + rotation.name() + "]@(" + x + "|" + y + ")#" + hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) { return true; }
+		if (o == null || getClass() != o.getClass()) { return false; }
+
+		Car car = (Car) o;
+
+		if (x != car.x) { return false; }
+		if (y != car.y) { return false; }
+		if (variant != car.variant) { return false; }
+		return rotation == car.rotation;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = x;
+		result = 31 * result + y;
+		result = 31 * result + (variant != null ? variant.hashCode() : 0);
+		result = 31 * result + (rotation != null ? rotation.hashCode() : 0);
+		return result;
+	}
 }
