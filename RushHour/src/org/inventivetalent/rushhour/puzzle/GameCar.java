08c917935719b5aa02cde4279fde79c0679da275
inventivetalent <inventivetalent@inventivegames.de>
2016-01-22 12:10:15 +0100

    add toString, equals and hashCode to GameCar

--- RushHour/src/org/inventivetalent/rushhour/puzzle/GameCar.java
+++ RushHour/src/org/inventivetalent/rushhour/puzzle/GameCar.java
@@ -6,11 +6,34 @@ import org.inventivetalent.rushhour.car.Variant;
 
 public class GameCar extends Car {
 
-//	public int currentX, currentY;
+	//	public int currentX, currentY;
 	public Bounds bounds;
 
 	public GameCar(Variant variant, Rotation rotation) {
 		super(variant, rotation);
 	}
 
+	@Override
+	public String toString() {
+		return "GameCar[v=" + variant.name() + "]@[(" + (int) bounds.getMinX() + "|" + (int) bounds.getMinY() + ")-(" + (int) bounds.getMaxX() + "|" + (int) bounds.getMaxY() + ")]#" + hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) { return true; }
+		if (o == null || getClass() != o.getClass()) { return false; }
+		if (!super.equals(o)) { return false; }
+
+		GameCar car = (GameCar) o;
+
+		return !(bounds != null ? !bounds.equals(car.bounds) : car.bounds != null);
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + (bounds != null ? bounds.hashCode() : 0);
+		return result;
+	}
 }
