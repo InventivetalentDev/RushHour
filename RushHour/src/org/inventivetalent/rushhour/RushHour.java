462162f7036ca4003b36cf169f8c63db42edeb82
inventivetalent <inventivetalent@inventivegames.de>
2016-01-22 11:48:34 +0100

    fix method to save puzzles

--- RushHour/src/org/inventivetalent/rushhour/RushHour.java
+++ RushHour/src/org/inventivetalent/rushhour/RushHour.java
@@ -1,5 +1,6 @@
 package org.inventivetalent.rushhour;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,6 +9,9 @@ import org.inventivetalent.rushhour.puzzle.generator.inventory.InventoryGenerato
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class RushHour extends JavaPlugin {
 
@@ -18,16 +22,20 @@ public class RushHour extends JavaPlugin {
 		saveDefaultConfig();
 
 		if (!puzzleFolder.exists()) {
-			puzzleFolder.mkdirs();
+			//			puzzleFolder.mkdirs();
 
 			//Save the included puzzles
-			saveResource("/puzzles", false);
+			try {
+				copyResource("puzzles");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		InventoryGenerator generator = new InventoryGenerator();
+		final InventoryGenerator generator = new InventoryGenerator();
 		generator.generateBase();
 		try {
 			generator.loadPuzzle(new File(puzzleFolder, "original/2.rh"));
@@ -35,7 +43,33 @@ public class RushHour extends JavaPlugin {
 			e.printStackTrace();
 		}
 
+		Bukkit.getScheduler().runTaskLater(this, new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Solving puzzle...");
+				generator.puzzle.solution.solve(generator.puzzle, 20);
+			}
+		}, 80);
+
 		generator.showTo((Player) sender);
 		return false;
 	}
+
+	//https://stackoverflow.com/questions/11012819/how-can-i-get-a-resource-folder-from-inside-my-jar-file
+	public void copyResource(String path) throws IOException {
+		final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+
+		if (jarFile.isFile()) {  // Run with JAR file
+			final JarFile jar = new JarFile(jarFile);
+			final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+			while (entries.hasMoreElements()) {
+				final String name = entries.nextElement().getName();
+				if (name.startsWith(path + "/")) { //filter according to the path
+					saveResource(name, false);
+				}
+			}
+			jar.close();
+		}
+	}
+
 }
