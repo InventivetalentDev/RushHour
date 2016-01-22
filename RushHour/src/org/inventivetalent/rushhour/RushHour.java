219f048b67ac44988a0ac1c62baa25456e4cb791
inventivetalent <inventivetalent@inventivegames.de>
2016-01-22 09:53:16 +0100

    save included puzzles

--- RushHour/src/org/inventivetalent/rushhour/RushHour.java
+++ RushHour/src/org/inventivetalent/rushhour/RushHour.java
@@ -19,6 +19,9 @@ public class RushHour extends JavaPlugin {
 
 		if (!puzzleFolder.exists()) {
 			puzzleFolder.mkdirs();
+
+			//Save the included puzzles
+			saveResource("/puzzles", false);
 		}
 	}
 
