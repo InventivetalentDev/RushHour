1ef7722a0486420baf93d0eb44687cc362c718c0
inventivetalent <inventivetalent@inventivegames.de>
2016-01-23 15:34:29 +0100

    remove test listeners and debug messages

--- RushHour/src/org/inventivetalent/rushhour/listener/InventoryListener.java
+++ RushHour/src/org/inventivetalent/rushhour/listener/InventoryListener.java
@@ -34,8 +34,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.metadata.MetadataValue;
-import org.inventivetalent.rushhour.event.PlayerBeginPuzzleEvent;
-import org.inventivetalent.rushhour.event.PlayerFinishPuzzleEvent;
 import org.inventivetalent.rushhour.puzzle.generator.AbstractPuzzleGenerator;
 import org.inventivetalent.rushhour.puzzle.generator.inventory.InventoryGenerator;
 
@@ -48,48 +46,16 @@ public class InventoryListener implements Listener {
 		Player player = (Player) event.getPlayer();
 		Inventory inventory = event.getInventory();
 
-		System.out.println("InventoryClose");
-
 		if (player.hasMetadata("RUSHHOUR_GENERATOR")) {
-			System.out.println("has meta");
-
 			List<MetadataValue> values = player.getMetadata("RUSHHOUR_GENERATOR");
 			if (values.isEmpty()) {
 				return;
 			}
 			AbstractPuzzleGenerator generator = (AbstractPuzzleGenerator) values.get(0).value();
-			System.out.println(generator);
 			if (generator instanceof InventoryGenerator) {
 				((InventoryGenerator) generator).puzzle.puzzleFinished(false);
 			}
 		}
-	}
-
-	//TODO: Remove test events
-
-	@EventHandler
-	public void onBegin(PlayerBeginPuzzleEvent event) {
-		System.out.println(event);
-		System.out.println(event.getPlayer());
-		System.out.println(event.getPuzzle());
-	}
-
-	@EventHandler
-	public void onFinish(PlayerFinishPuzzleEvent event) {
-		System.out.println(event);
-		System.out.println(event.getPlayer());
-		System.out.println(event.getPuzzle());
-		System.out.println(event.getMoves());
-		System.out.println(event.isSolved());
-		System.out.println(event.isUsedSolution());
-
-		System.out.println("Player Solution: ");
-		System.out.println(event.getPlayerSolution().moves);
-		System.out.println(event.getPlayerSolution().combineMoves().moves);
-		System.out.println("in " + event.getPlayerSolution().getDuration() + "ms");
-
-		System.out.println("Original Solution: ");
-		System.out.println(event.getPuzzleSolution().moves);
 	}
 
 }
